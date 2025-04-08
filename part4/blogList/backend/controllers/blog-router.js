35aadf51fb69969f458436ddad14ee02e7c9f05f
Sanketh S G <sankethsg2944@gmail.com>
2025-04-08 11:40:43 +0200

    exercise 4.13-4.14 completed

--- part4/blogList/backend/controllers/blog-router.js
+++ part4/blogList/backend/controllers/blog-router.js
@@ -15,19 +15,62 @@ blogRouter.get('/', (request, response) => {
       })
   })
   
-  blogRouter.post('/', (request, response) => {
-    // const body = request.body
-    // logger.info('body', body)
+blogRouter.post('/', (request, response) => {
+  // const body = request.body
+  // logger.info('body', body)
 
-    const blog = new Blog(request.body)
-    if (!blog.title || !blog.url || blog.title === '' || blog.url === '') {
-      return response.status(400).json({ error: 'title or url missing' })
-    }
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+  const blog = new Blog(request.body)
+  if (!blog.title || !blog.url || blog.title === '' || blog.url === '') {
+    return response.status(400).json({ error: 'title or url missing' })
+  }
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+blogRouter.delete('/:id', (request, response) => {
+  const id = request.params.id
+  Blog
+    .findByIdAndDelete(id)
+    .then(result => {
+      if (result) {
+        response.status(204).end()
+      } else {
+        response.status(404).json({ error: 'blog not found' })
+      }
+    })
+    .catch(error => {
+      logger.error('Error deleting blog:', error.message)
+      response.status(500).json({ error: 'internal server error' })
+    })
+})
+
+blogRouter.put('/:id', (request, response) => {
+  const id = request.params.id  
+  const body = request.body
+  Blog.findById(id)
+    .then(blog => {
+      if (!blog) {
+        return response.status(404).json({ error: 'blog not found' })
+      }
+      blog.title = body.title 
+      blog.author = body.author 
+      blog.url = body.url 
+      blog.likes = body.likes
+
+      return blog.save()
+    })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+    .catch(error => {
+      logger.error('Error updating blog:', error.message)
+      response.status(500).json({ error: 'internal server error' })
+    })
+})
+
+
 
   module.exports = blogRouter
--- part4/blogList/backend/tests/blog_api.test.js
+++ part4/blogList/backend/tests/blog_api.test.js
@@ -1,14 +1,13 @@
 // tests/list_helper.test.js
-const { beforeEach, test, describe, after } = require('node:test');
+const { test, describe, after } = require('node:test');
 const assert = require('node:assert');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
 const listHelper = require('../utils/list_helper');
-const Blog = require('../models/blog');
 const blogsHelper = require('./blogs_helper');
-const { title } = require('node:process');
+
 
 const blogs = blogsHelper.initBlogs;
 
@@ -124,6 +123,39 @@ test("POST /api/blogs without title and url returns 400", async () => {
     .send(newBlog)
     .expect(400);
 });
+
+test.only('DELETE /api/blogs/:id removes a blog', async () => {
+  const blogsAtStart = await blogsHelper.blogsInDb();
+  // console.log('blogsAtStart', blogsAtStart.length)
+  const blogToDelete = blogsAtStart[0];
+
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204);
+
+  const blogsAtEnd = await blogsHelper.blogsInDb();
+  // console.log('blogsAtEnd', blogsAtEnd.length)
+  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length - 1);
+
+});
+
+test.only('PUT /api/blogs/:id updates a blog', async () => {
+  const blogsAtStart = await blogsHelper.blogsInDb();
+  const blogToUpdate = blogsAtStart[0];
+  const updatedBlog = { ...blogToUpdate, likes: blogToUpdate.likes + 1 };
+
+
+  await api
+    .put(`/api/blogs/${blogToUpdate.id}`)
+    .send(updatedBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await blogsHelper.blogsInDb();
+  const updatedBlogFromDb = blogsAtEnd.find((blog) => blog.id === blogToUpdate.id);
+  assert.strictEqual(updatedBlogFromDb.likes, updatedBlog.likes);
+});
+
 
 
 after(async () => {
